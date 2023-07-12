57c6052c4172be938564bd758fc5b7d457a5b35e
Joyo Kilat Moko <joyokilatm@gmail.com>
2023-07-13 03:14:01 +0700

    sudah bisa menggunakan login dengan SharedPreference, sederhana saja

--- app/src/main/java/com/example/agile/ActivityLogin.kt
+++ app/src/main/java/com/example/agile/ActivityLogin.kt
@@ -1,38 +1,40 @@
 package com.example.agile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.agile.util.pref
 
 class ActivityLogin : AppCompatActivity() {
     private lateinit var etUsername: EditText
     private lateinit var etPassword: EditText
-    private lateinit var btnLogin: Button
-
-    private lateinit var pref: pref
+    private lateinit var buttonLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        pref = pref(this)
-
         etUsername = findViewById(R.id.etUsername)
         etPassword = findViewById(R.id.etPassword)
-        btnLogin = findViewById(R.id.btnLogin)
+        buttonLogin = findViewById(R.id.btnLogin)
 
-        btnLogin.setOnClickListener {
-            val username = etUsername.text.toString().trim()
-            val password = etPassword.text.toString().trim()
+        buttonLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
 
-            if (username == "Joyokilatmoko" && password == "joyokilatmoko123") {
-                pref.setISlogin(true)
-                // Lanjutkan ke halaman selanjutnya atau lakukan tindakan lain setelah login berhasil
+            if (username == "joyo" && password == "123456") {
+                val s = pref(this)
+                s.setISlogin(true)
+                Toast.makeText(this, "Login Berhasil", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
             } else {
-                pref.setISlogin(false)
-                // Tampilkan pesan login gagal atau lakukan tindakan lain jika login gagal
+                Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/example/agile/MainActivity.kt
+++ app/src/main/java/com/example/agile/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.agile
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.HorizontalScrollView
+import android.widget.ScrollView
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -25,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("SuspiciousIndentation")
+    @SuppressLint("SuspiciousIndentation", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
@@ -53,18 +57,21 @@ class MainActivity : AppCompatActivity() {
                 val s = pref(this)
                 if (s.getISlogin()) {
                     Log.d("TAG", "SUDAH LOGIN")
+                    // Navigasi ke halaman navigation_akun
+                    navController.navigate(R.id.navigation_akun)
+                    true
                 } else {
-                    //startActivity(Intent(this,LoginActivity::class.java))
                     startActivity(Intent(this, ActivityLogin::class.java))
-                    Log.d("TAG", "Belum Login pindah ke hal login")
+                    Log.d("TAG", "Belum Login, pindah ke halaman login")
+                    false
                 }
-                true
             } else {
                 Log.d("TAG", "onCreate yg Lain" + item.itemId)
                 navController.navigate(item.itemId)
                 true
             }
         }
+
 
 
 
@@ -78,10 +85,36 @@ class MainActivity : AppCompatActivity() {
         }
 
 //------------membuka aktivity kota nelpon-----------
-        val btnCall=findViewById<Button>(R.id.btn_call)
-        btnCall.setOnClickListener {
-            val intent = Intent(this, activity_kuota_call::class.java)
+//        val btnCall=findViewById<Button>(R.id.btn_call)
+//        btnCall.setOnClickListener {
+//            val intent = Intent(this, activity_kuota_call::class.java)
+//            startActivity(intent)
+//        }
+        // Pada Activity yang sesuai, misalnya MainActivity
+        val button = findViewById<Button>(R.id.btn_call)
+        button.setOnClickListener {
+            val s = pref(this)
+            if (s.getISlogin()) {
+                // Jika sudah login, langsung navigasi ke ActivityKuotaCall
+                startActivity(Intent(this, activity_kuota_call::class.java))
+            } else {
+                // Jika belum login, pindah ke halaman login terlebih dahulu
+                startActivity(Intent(this, ActivityLogin::class.java))
+            }
+        }
+
+//        logout
+        val btnExit = findViewById<Button>(R.id.btn_baru)
+        btnExit.setOnClickListener {
+            val pref = getSharedPreferences("Mypref", Context.MODE_PRIVATE)
+            val editor = pref.edit()
+            editor.clear()
+            editor.apply()
+
+            // Pindah ke Fragment Home setelah logout
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish() // Mengakhiri activity saat ini agar tidak bisa kembali tanpa login ulang
         }
 //        --------------ini embuka aktivity kuota internet------
         val btnGB=findViewById<Button>(R.id.btn_gb)
@@ -96,6 +129,45 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, activity_pulsa::class.java)
             startActivity(intent)
         }
+
+//        nilai nilai isi
+        val pref = pref(this)
+        val txtpulsa = findViewById<TextView>(R.id.txtpulsa)
+        val koin = findViewById<TextView>(R.id.txtkoin)
+        val selamatdatang = findViewById<TextView>(R.id.txtSelamatDatang)
+        val sejak = findViewById<TextView>(R.id.sejak)
+        val nomor = findViewById<Button>(R.id.btn_toast_nomor)
+        val hari = findViewById<TextView>(R.id.tvhari)
+        val level = findViewById<TextView>(R.id.tvlevel)
+        val scrhorizontal = findViewById<HorizontalScrollView>(R.id.scrhorizontal)
+
+        if (pref.getISlogin()) {
+            // Jika sudah login
+            txtpulsa.text = "Rp.50000"
+            koin.text = "240 Poin"
+            selamatdatang.text = "Selamat Datang, Joyo!"
+            sejak.text = "Sejauh ini kamu sudah membeli hingga Rp.360.000!"
+            nomor.text = "082297068911"
+            hari.text = "187"
+            level.text = "12"
+            scrhorizontal.visibility = View.VISIBLE
+
+        } else {
+            // Jika belum login
+            txtpulsa.text = "Rp.0"
+            koin.text = "0 Poin"
+            selamatdatang.text = "Harap Login!"
+            sejak.text = "_"
+            nomor.text = "-"
+            hari.text = "-"
+            level.text = "-"
+            scrhorizontal.visibility = View.GONE
+
+        }
+
+
+
+
 //        -----------------ini batas akhir---------
 
     }
